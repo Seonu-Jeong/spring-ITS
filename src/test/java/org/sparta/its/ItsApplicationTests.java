1ad55447f007432f786f802ab919a261fb17f999
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-07 17:21:53 +0900

    init project

---
+++ src/test/java/org/sparta/its/ItsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.its;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
