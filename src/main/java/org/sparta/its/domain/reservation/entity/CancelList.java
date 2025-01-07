81a2bcbbdd00c1482659e6bb3c9ced1b84b2400a
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-07 20:45:36 +0900

    add :  CancelList Entity 생성

---
+++ src/main/java/org/sparta/its/domain/reservation/entity/CancelList.java
@@ -0,0 +1,44 @@
+package org.sparta.its.domain.reservation.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.sparta.its.domain.user.entity.User;
+import org.sparta.its.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "cancel_list")
+@NoArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class CancelList extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// 연관관계
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	// 필드
+	@Column(length = 40)
+	private String description;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 10)
+	private CancelStatus status;
+}
