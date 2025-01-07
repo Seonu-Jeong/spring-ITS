d48ee664e2c8853ecd18672722f517cee9cd5396
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-07 20:46:40 +0900

    update : Concert Entity 연관관계 추가

--- src/main/java/org/sparta/its/domain/concert/entity/Concert.java
+++ src/main/java/org/sparta/its/domain/concert/entity/Concert.java
@@ -2,14 +2,22 @@ package org.sparta.its.domain.concert.entity;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.DynamicUpdate;
+import org.sparta.its.domain.hall.entity.Hall;
+import org.sparta.its.domain.reservation.entity.Reservation;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,6 +31,18 @@ public class Concert {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	// 연관관계
+	@OneToMany(mappedBy = "concert")
+	private List<ConcertImage> concertImages = new ArrayList<>();
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "hall_id")
+	private Hall hall;
+
+	@OneToMany(mappedBy = "concert")
+	private List<Reservation> reservations = new ArrayList<>();
+
+	// 필드
 	@Column(nullable = false, length = 30)
 	private String title;
 
