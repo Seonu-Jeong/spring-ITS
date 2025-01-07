1658fd9f6212ba632c4279716dd94ea2039efc58
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-07 19:31:56 +0900

    add : ConcertImage Entity 생성

---
+++ src/main/java/org/sparta/its/domain/concert/entity/ConcertImage.java
@@ -0,0 +1,29 @@
+package org.sparta.its.domain.concert.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "concert_image")
+@NoArgsConstructor
+public class ConcertImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "concert_id")
+	private Concert concert;
+
+	@Column(nullable = false, length = 255)
+	private String imageUrl;
+}
