8b5416d36c2cb0d60f89a57ef879965e68e9a18f
jut <jangutm600.gmail.com>
2025-01-09 17:07:19 +0900

    add : HallRepository default 메서드 생성

--- src/main/java/org/sparta/its/domain/hall/repository/HallRepository.java
+++ src/main/java/org/sparta/its/domain/hall/repository/HallRepository.java
@@ -1,6 +1,8 @@
 package org.sparta.its.domain.hall.repository;
 
 import org.sparta.its.domain.hall.entity.Hall;
+import org.sparta.its.global.exception.HallException;
+import org.sparta.its.global.exception.errorcode.HallErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HallRepository extends JpaRepository<Hall, Long> {
@@ -11,4 +13,8 @@ public interface HallRepository extends JpaRepository<Hall, Long> {
 	// @Query 작성 메소드
 
 	// Default 메소드
+	default Hall findByIdOrThrow(Long hallId) {
+		return findById(hallId).orElseThrow(() ->
+			new HallException(HallErrorCode.EXAMPLE));
+	}
 }
