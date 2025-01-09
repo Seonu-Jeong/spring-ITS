8d5625a08db8f48b03a9ee14871c5500212efe2a
kimuky <xogus559@gmail.com>
2025-01-09 12:00:17 +0900

    add: 성능 개선 추가 부분 구현

---
+++ src/main/java/org/sparta/its/domain/hall/repository/HallBulkRepository.java
@@ -0,0 +1,46 @@
+package org.sparta.its.domain.hall.repository;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HallBulkRepository {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public HallBulkRepository(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public void saveAllHallImage(Long hallId, List<String> urls) {
+		String sql = "INSERT INTO hall_image (hall_id, image_url) " +
+			"VALUES (?,?)";
+
+		jdbcTemplate.batchUpdate(sql,
+			urls,
+			urls.size(),
+			(PreparedStatement ps, String url) -> {
+				ps.setLong(1, hallId);
+				ps.setString(2, url);
+			});
+	}
+
+	public void saveAllSeat(Long hallId, List<Integer> seatNumbers) {
+		String sql = "INSERT INTO Seat (hall_id, seat_number) " +
+			"VALUES (?,?)";
+
+		jdbcTemplate.batchUpdate(sql,
+			seatNumbers,
+			seatNumbers.size(),
+			(PreparedStatement ps, Integer seatNumber) -> {
+				ps.setLong(1, hallId);
+				ps.setInt(2, seatNumber);
+			});
+	}
+
+}
