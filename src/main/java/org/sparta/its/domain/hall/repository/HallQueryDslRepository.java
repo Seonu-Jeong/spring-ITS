16cc8fc34df13914f30e4ea0113096f57641d933
kimuky <xogus559@gmail.com>
2025-01-10 11:44:21 +0900

    fix: 반환 Page<dto> -> List<dto>로 수정

--- src/main/java/org/sparta/its/domain/hall/repository/HallQueryDslRepository.java
+++ src/main/java/org/sparta/its/domain/hall/repository/HallQueryDslRepository.java
@@ -1,9 +1,9 @@
 package org.sparta.its.domain.hall.repository;
 
-import org.sparta.its.domain.hall.dto.HallResponse;
+import org.sparta.its.domain.hall.entity.Hall;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface HallQueryDslRepository {
-	Page<HallResponse.ReadDto> findByNameAndLocation(String name, String location, Pageable pageable);
+	Page<Hall> findByNameAndLocation(String name, String location, Pageable pageable);
 }
--- src/main/java/org/sparta/its/domain/hall/repository/HallRepositoryImpl.java
+++ src/main/java/org/sparta/its/domain/hall/repository/HallRepositoryImpl.java
@@ -1,17 +1,15 @@
 package org.sparta.its.domain.hall.repository;
 
 import static org.sparta.its.domain.hall.entity.QHall.*;
-import static org.sparta.its.domain.hall.entity.QHallImage.*;
 
 import java.util.List;
 
-import org.sparta.its.domain.hall.dto.HallResponse;
+import org.sparta.its.domain.hall.entity.Hall;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -28,29 +26,17 @@ public class HallRepositoryImpl implements HallQueryDslRepository {
 	}
 
 	/**
-	 *
+	 * TODO	N+1 문제 해결해야함
 	 * @param name 공연장 이름
 	 * @param location 공연장 위치
 	 * @param pageable page = 1, 2, 3 ...번 페이지 번호, size = 페이지 마다 몇 개의 데이터
 	 * @return
 	 */
 	@Override
-	public Page<HallResponse.ReadDto> findByNameAndLocation(String name, String location, Pageable pageable) {
-		List<HallResponse.ReadDto> fetch = jpaQueryFactory.select(
-				Projections.constructor(HallResponse.ReadDto.class,
-					hall.id,
-					hall.name,
-					hall.location,
-					hall.capacity,
-					hall.createdAt,
-					hall.modifiedAt,
-					hallImage.imageUrl,
-					hall.isOpen))
-			.from(hall)
-			.leftJoin(hall.hallImages, hallImage)
+	public Page<Hall> findByNameAndLocation(String name, String location, Pageable pageable) {
+		List<Hall> fetch = jpaQueryFactory.selectFrom(hall)
 			.where(hallNameLike(name), HallLocationLike(location))
-			.groupBy(hall)
-			.orderBy(hall.isOpen.desc(), hall.name.desc())
+			.orderBy(hall.isOpen.desc(), hall.name.asc())
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
 			.fetch();
