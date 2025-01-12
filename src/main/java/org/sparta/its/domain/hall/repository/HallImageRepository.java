78bbf177f1bc0beff3ac7f1f1c02c386059fda90
kimuky <xogus559@gmail.com>
2025-01-13 04:08:37 +0900

    add: 공연장 이미지 디폴트 메서드 추가

--- src/main/java/org/sparta/its/domain/hall/repository/HallImageRepository.java
+++ src/main/java/org/sparta/its/domain/hall/repository/HallImageRepository.java
@@ -1,6 +1,8 @@
 package org.sparta.its.domain.hall.repository;
 
 import org.sparta.its.domain.hall.entity.HallImage;
+import org.sparta.its.global.exception.HallImageException;
+import org.sparta.its.global.exception.errorcode.HallImageErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HallImageRepository extends JpaRepository<HallImage, Long> {
@@ -10,4 +12,8 @@ public interface HallImageRepository extends JpaRepository<HallImage, Long> {
 	// @Query 작성 메소드
 
 	// Default 메소드
+	default HallImage findByIdOrThrow(Long id) {
+		return findById(id)
+			.orElseThrow(() -> new HallImageException(HallImageErrorCode.NOT_FOUND_HALL_IMAGE));
+	}
 }
