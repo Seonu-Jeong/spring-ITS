261daf0c8419a2a6e428566e55ee71f41e84f2e8
Seonu-Jeong <sw.jeong97@gmail.com>
2025-01-12 19:40:55 +0900

    update : 유저 삭제 API 추가

--- src/main/java/org/sparta/its/domain/user/controller/UserController.java
+++ src/main/java/org/sparta/its/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.sparta.its.global.security.UserDetail;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,22 @@ public class UserController {
 		@Valid @RequestBody UserRequest.UpdateDto updateDto,
 		@AuthenticationPrincipal UserDetail userDetail) {
 		UserResponse.UpdateDto responseDto = userService.updateUser(updateDto, userDetail.getId());
+
+		return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+	}
+
+	/**
+	 * 유저 회원탈퇴 API
+	 * @param deleteDto {@link Valid} {@link RequestBody} 유저 삭제 DTO
+	 * @param userDetail {@link AuthenticationPrincipal} 유저 인증 객체
+	 * @return {@link ResponseEntity} HttpStatus 상태값과 body 응답 {@link UserResponse.DeleteDto} 삭제 Dto 응답
+	 */
+	@DeleteMapping
+	public ResponseEntity<UserResponse.DeleteDto> deleteUser(
+		@Valid @RequestBody UserRequest.DeleteDto deleteDto,
+		@AuthenticationPrincipal UserDetail userDetail) {
+
+		UserResponse.DeleteDto responseDto = userService.deleteUser(deleteDto, userDetail.getId());
 
 		return ResponseEntity.status(HttpStatus.OK).body(responseDto);
 	}
