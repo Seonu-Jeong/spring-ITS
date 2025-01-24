0771f086a5f1fe2ca043df202fe2dd940419d2b9
Jun Heo <huhjune98@naver.com>
2025-01-24 17:56:57 +0900

    add: dumb data

---
+++ src/main/resources/sql/hall.sql
@@ -0,0 +1,2 @@
+insert into hall (id, name, location, capacity, isOpen) values (1, 'Rodriguez-Huel', 'Diang', 500, 'true');
+insert into hall (id, name, location, capacity, isOpen) values (2, 'Satterfield, Kutch and Spinka', 'Kasonawejo', 500, 'true');
